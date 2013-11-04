fa18c096384318f05f70ef966f0cdaec4b7bd356
airosB <airos.berlanathe@gmail.com>
2013-11-05 00:24:18 +0900

    added support for ST3, and some other features updated

--- multi_number_input.py
+++ multi_number_input.py
@@ -1,15 +1,32 @@
-import sublime_plugin
+import sublime, sublime_plugin
 
 class MultiNumberInputCommand(sublime_plugin.TextCommand):
-    def on_done(self, args):
-        args_array = args.split(" by ")
-        num = args_array[0]
-        by = int(args_array[1]) if 1 < len(args_array) else 1
-        edit = self.view.begin_edit()
-        for region in self.view.sel():
-            self.view.replace(edit, region, str(num))
-            num = int(num) + by
-        self.view.end_edit(edit)
+    def on_done(self, arg):
+        paramsArray = arg.split(" ")
+        paramsArray.extend(['', '']) #minimum length is 3
+        initial     = int(paramsArray[0]) if paramsArray[0] != '' else 1
+        digits      = int(paramsArray[1]) if paramsArray[1] != '' else 1
+        interval    = int(paramsArray[2]) if paramsArray[2] != '' else 1
+        self.view.run_command('insert_numbers', {'args': [initial, interval, digits]})
+
     def run(self, edit):
-        # Show input panel for surround word, then handling input
-        self.view.window().show_input_panel('Initial number ex) 1 by 3: ', '1', self.on_done, None, None)
+            self.view.window().show_input_panel('Initial Digits Step | ex) 2 5 3: ', '', self.on_done, None, None)
+
+
+class InsertNumbers(sublime_plugin.TextCommand):
+  def run(self, edit, args):
+    number = args[0]
+    interval = args[1]
+    digits = args[2]
+
+    for region in self.view.sel():
+        #for negative values
+        if number < 0:
+            my_digits = digits + 1
+        else:
+            my_digits = digits
+
+        formatter = '{0:0' + str(my_digits) + 'd}'
+        formatted = formatter.format(number)
+        self.view.replace(edit, region, str(formatted))
+        number = number + interval
